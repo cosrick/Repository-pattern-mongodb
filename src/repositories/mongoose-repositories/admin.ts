cdc68c0784f58abf43c540a238a33a15341a25b5
cosrick <rick830620@gmail.com>
2021-11-16 15:23:46 +0800

    Implement repository method

--- src/repositories/mongoose-repositories/admin.ts
+++ src/repositories/mongoose-repositories/admin.ts
@@ -21,4 +21,87 @@ export class AdminRepository extends BaseRepository<Admin> {
         });
     });
   };
+
+  public list(): Promise<Admin[]> {
+    return new Promise((resolve, reject) => {
+      this._model.find({})
+        .then((admins) => {
+          resolve(admins);
+        }).catch((error)=>{
+          reject(error);
+        });
+    });
+  }
+
+  public delete(id: string): Promise<Admin> {
+    return new Promise((resolve, reject) => {
+      this._model.findByIdAndDelete(id)
+        .then((deletedAdmin) => {
+          if (!deletedAdmin) {
+            reject(null);
+          } else {
+            resolve(deletedAdmin);
+          }
+        }).catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  public update(id: string): Promise<Admin> {
+    return new Promise((resolve, reject) => {
+      this._model.findByIdAndUpdate(id)
+        .then((updatedAdmin) => {
+          if (!updatedAdmin) {
+            reject(null);
+          } else {
+            resolve(updatedAdmin);
+          }
+        }).catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  public find(query: Partial<Admin>): Promise<Admin> {
+    return new Promise((resolve, reject) => {
+      this._model.findOne(query)
+        .then((admin) => {
+          if (!admin) {
+            reject(null);
+          } else {
+            resolve(admin);
+          }
+        }).catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  public findOne(id: string): Promise<Admin> {
+    return new Promise((resolve, reject) => {
+      this._model.findById(id)
+        .then((admin) => {
+          if (!admin) {
+            reject(null);
+          } else {
+            resolve(admin);
+          }
+        }).catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  public search(query: any): Promise<Admin[]> {
+    return new Promise((resolve, reject) => {
+      this._model.find(query)
+        .then((admins) => {
+          resolve(admins);
+        }).catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
 };
